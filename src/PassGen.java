ce7d77ae5481b046772aef4f5dca44792eb4b1df
Nadya22 <lemon13579@mail.ru>
2019-01-02 21:34:01 +0600

    Создана структура проекта

---
+++ src/PassGen.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class PassGen {
+
+    public String getPassword(int size) {
+        StringBuilder b = new StringBuilder();
+        Random r = new Random();
+        for (int i = 0; i < size; i++)
+            b.append((char) (r.nextInt(26) + 'a'));
+            return b.toString();
+    }
+}
